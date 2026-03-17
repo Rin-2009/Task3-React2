45f550249a87ab8085b312237b27772646d362c2
Rin-2009 <lyfay6768@gmail.com>
2026-03-17 14:58:36 +0300

    added Navbar and slider backgrounds of the part1

---
+++ Task3_React2.jsx/Home.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import Navbar from "./Navbar";
+
+function Home() {
+  const backgrounds = [
+    { id: 1, src: "https://j.top4top.io/p_3727atiwv0.jpg" },
+    { id: 2, src: "https://h.top4top.io/p_37273zity0.jpg" },
+    { id: 3, src: "https://h.top4top.io/p_37276z5lb0.jpg" },
+    { id: 4, src: "https://h.top4top.io/p_37276z5lb0.jpg" },
+    { id: 5, src: "https://d.top4top.io/p_37270kygs0.jpg" },
+    { id: 6, src: "https://g.top4top.io/p_372730bv60.jpg" },
+    { id: 7, src: "https://h.top4top.io/p_37277qqcz0.jpg" },
+    { id: 8, src: "https://h.top4top.io/p_37277qqcz0.jpg" },
+    { id: 9, src: "https://f.top4top.io/p_3727jimb50.jpg" },
+  ];
+
+  const [background, setBackground] = useState(0);
+
+  const [lightMood, setLightMood] = useState(false);
+
+  const ThemLight = () => {
+    setLightMood(!lightMood);
+  };
+
+  const [color, setColor] = useState("#7d7d96fb");
+
+  const changeColor = (e) => {
+    setColor(e.target.value);
+  };
+
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${backgrounds[background].src})`;
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.body.style.height = "100vh";
+    document.body.style.width = "100vw";
+    document;
+  }, [background]);
+
+  return (
+
+    <>
+
+    <div style={{
+        position:"fixed",
+        top:"0",
+        left:"0",
+        width:"100%",
+        height:"100%",
+        backgroundColor: lightMood ? "rgba(252, 246, 246, 0.08)" : "rgba(0, 0, 0, 0.6)",
+        zIndex:-1
+    }}>
+
+    </div>
+      <Navbar
+        backgrounds={backgrounds}
+        background={background}
+        setBackground={setBackground}
+        ThemLight= {ThemLight}
+        lightMood={lightMood}
+        setLightMood={setLightMood}
+        color={color}
+        setColor={setColor}
+        changeColor={changeColor}
+      />
+    </>
+  );
+}
+
+export default Home;
